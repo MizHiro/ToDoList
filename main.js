2604007e0e446ff336b08c68029ee9576b95242b
MizHiro <pencil3274@gmail.com>
2021-03-09 01:18:10 +0900

    delete unnecessary comments.

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
 const app = new Vue({
   el: "#app",
   data: {
-    // 初期値
     newItem: '',
     todos: []
   },
 
   methods: {
-    // ①アイテムの追加→入力欄の初期化
     addItem: function(event) {
-      // 未入力=>追加しない
+      // 未入力である場合
       if (this.newItem === '') return;
-      // ---------------------------------
-      // 新しい変数todoに入力されたnewItemを追加(elseに該当)
+
       let todo = {
         item: this.newItem,
         isDone: false
       };
-      // 既に定義されているtodos配列へtodoを格納
+
       this.todos.push(todo);
-      // 追加後テキストボックスを空白にする
+
       this.newItem = '';
     },
 
-    // ②指定アイテムの消去
+    // index=todos[]のkey
     deleteItem: function(index) {
       this.todos.splice(index, 1)
     }
